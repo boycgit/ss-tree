41a2b640fa36c4d047d8acdacaa192bacef12473
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2018-11-16 12:59:58 +0800

    fix(bugfix): export more class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 
 export * from './node/treenode';
+export * from './node/binarytreenode';
+export * from './node/traverse';
 export * from './tree/tree';
+export * from './binary-search-tree/index';
